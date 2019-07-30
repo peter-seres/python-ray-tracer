Can now trace multiple objects. Fixed 90 degree rotation of .png fromarray.

camera.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sphere import Sphere
 
 
 def enum_pixel_array(array):
@@ -90,15 +91,29 @@ class Camera:
 
                 yield j, k, ray, pix_loc_global
 
-    def cast_rays(self, sphere):
-
+    def cast_rays(self, object_list):
         pixel_array = np.zeros(shape=(len(self.yy), len(self.zz), 3), dtype=np.uint8)
 
         # Loop through the pixel locations and cast rays:
         for j, k, ray, _ in self.generate_rays():
-            pixel_array[j, k] = sphere.intersect_ray(ray)
 
-        return pixel_array
+            dist = np.inf
+            closes_obj_index = 0
+            for obj_index, obj in enumerate(object_list):
+                dist_ray = obj.intersect_ray(ray)
+
+                if dist_ray < dist:
+                    dist = dist_ray
+                    closes_obj_index = obj_index
+
+            if dist == np.inf:
+                color = np.array([0, 0, 0])
+            else:
+                color = object_list[closes_obj_index].color
+
+            pixel_array[j, k] = color
+
+        return np.rot90(pixel_array)
 
 
 

main.py
@@ -1,48 +1,31 @@
 import time
-
 from PIL import Image
-import numpy as np
 from camera import Camera
-
-
-class Sphere:
-    def __init__(self, position, radius, color):
-        self.origin = np.array(position)
-        self.r = radius
-        self.color = np.array(color)
-
-    def intersect_ray(self, ray):
-
-        a = np.dot(ray.dir, ray.dir)
-        b = 2 * np.dot(ray.dir, ray.origin - self.origin)
-        c = np.dot(ray.origin - self.origin, ray.origin - self.origin) - self.r * self.r
-
-        discriminant = b * b - 4 * a * c
-
-        if discriminant < 0:
-            return np.array([0, 0, 0])
-        else:
-            return self.color
+from sphere import Sphere
 
 
 def main():
     # Image resolution
-    w = 500     # image width (pixels)
-    h = 480     # image height (pixels)
+    w = 300     # image width (pixels)
+    h = 300     # image height (pixels)
 
     # The scene contains a camera and a sphere:
-    camera = Camera(w, h, origin=[0, 0, 0], direction=[1, 0, 1], field_of_view=45)
-    sphere1 = Sphere(position=[50, 0, 0], radius=5, color=[255, 255, 123])
+    camera = Camera(w, h, origin=[0, -5, 0], field_of_view=45)
+    sphere1 = Sphere(position=[50, -5, 0], radius=3.5, color=[255, 0, 0])
+    sphere2 = Sphere(position=[50, 5, 0], radius=3.5, color=[0, 255, 0])
+    sphere3 = Sphere(position=[70, 5, 0], radius=10, color=[0, 0, 255])
+
+    object_list = [sphere1, sphere2, sphere3]
 
     # Generate pixels:
     start = time.time()
-    pixel_array = camera.cast_rays(sphere1)
+    pixel_array = camera.cast_rays(object_list)
     end = time.time()
-    print(f'Ray tracing took {end-start} seconds with {500*480} rays and 1 object.')
+    print(f'Ray tracing took {end-start} seconds with {500*480} rays and {len(object_list)} objects.')
 
     # Save the data to a png file:
     image = Image.fromarray(pixel_array)
-    image.save('look_up.png')
+    image.save('result.png')
 
 
 if __name__ == '__main__':