61400c9fa479942d74e479032685aa69784215a3
Speterius <peet.seres@gmail.com>
2019-07-31 17:22:02 +0200

    Restructured the ray direction generation. Rendering now takes 1.568 microsec per pixel

--- clean.py
+++ clean.py
@@ -2,19 +2,17 @@ from dataclasses import dataclass
 import numpy as np
 from PIL import Image
 import time
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
 from functools import wraps
-from time import time
 
 
 def timed(f):
     @wraps(f)
     def wrap(*args, **kw):
-        start = time()
+        start = time.time()
         result = f(*args, **kw)
-        end = time()
-        print('func:%r args:[%r, %r] took: %2.4f sec' % (f.__name__, args, kw, end-start))
+        end = time.time()
+        print(f'Time test function {f.__name__}: with arguments:[{args}, {kw}]')
+        print(f'Duration: ', '{:10.10f}'.format(end-start))
         return result
     return wrap
 
@@ -34,38 +32,15 @@ class Ray:
 
 @dataclass
 class Camera:
-    origin: np.ndarray
     field_of_view: float
 
-    # todo: Allow camera coordinate system to be rotated
+    # todo: Allow camera coordinate system to be moved and rotated
+
+    origin: np.ndarray = np.array([0, 0, 0])
     cam_i: np.ndarray = np.array([1, 0, 0])
     cam_j: np.ndarray = np.array([0, -1, 0])
     cam_k: np.ndarray = np.array([0, 0, 1])
     R = np.array([cam_i, cam_j, cam_k])
-
-    def generate_pixel_locations(self, width, height):
-        # Generate pixel locations in camera-body coordinate-frame:
-        x = 1 / np.tan(np.radians(self.field_of_view)/2)    # Distances of pixels from camera origin
-        yy = np.linspace(-1, 1, width)                      # Distances of pixels along width
-        zz = np.linspace(-1, 1, height)                     # Distances of pixels along height
-
-        # Loop through the pixel locations and to generate a ray for pixel location j, k
-        for j, y in enumerate(yy):
-            for k, z in enumerate(zz):
-                pix_loc_local = [x, y, z]
-                pix_loc_global = self.origin + np.matmul(self.R, np.transpose(pix_loc_local))
-
-                yield pix_loc_global, j, k
-
-    def generate_rays(self, width, height):
-        for pix_loc_global, j, k in self.generate_pixel_locations(width, height):
-
-            ray_direction = (pix_loc_global - self.origin)
-            ray_direction /= np.linalg.norm(ray_direction)
-
-            ray = Ray(origin=self.origin, dir=ray_direction)
-
-            yield ray, j, k
 
     def enum_coordinate_system(self):
         for vec in [self.cam_i, self.cam_j, self.cam_k]:
@@ -82,40 +57,65 @@ class Camera:
                 pix_loc_local = [x, y, z]
                 yield self.origin + np.matmul(self.R, np.transpose(pix_loc_local))
 
+    def dir(self, loc):
+
+        # todo: Right now this assumes that the camera.origin is the same as global coordinate system origin
+        direction = np.matmul(self.R, np.transpose(loc))
+        return direction
+
+    def create_rays(self, width, height):
+
+        # Pixel locations in local coord system:
+        PIX_LOC = np.zeros(shape=(3, width, height))                                    # Allocate numpy 3D array
+        yy, zz = np.mgrid[-1:1:complex(0, width), -1:1:complex(0, height)]              # Create pixel grid
+        xx = np.ones(shape=yy.shape) * (1 / np.tan(np.radians(self.field_of_view)/2))   # Distance of grid from origin
+        PIX_LOC[0, :, :] = xx
+        PIX_LOC[1, :, :] = yy
+        PIX_LOC[2, :, :] = zz
+
+        # Calculate ray direction vectors:
+        RD = np.apply_along_axis(self.dir, 0, PIX_LOC)
+
+        return RD
+
 
 class Scene:
     def __init__(self):
         self.objects = []
-        self.camera = Camera(origin=np.array([0, 0, 0]), field_of_view=45)
+        self.camera = Camera(field_of_view=45)
+        self.background = np.array([25, 25, 25])
 
     def add_object(self, obj):
         self.objects.append(obj)
 
+    def trace(self, ray_direction):
+        ray_origin = self.camera.origin
+
+        dist = np.inf
+        closes_obj_index = 0
+        for obj_index, obj in enumerate(self.objects):
+            dist_ray = intersect_ray_sphere(ray_origin, ray_direction, obj.origin, obj.radius)
+
+            if dist_ray < dist:
+                dist = dist_ray
+                closes_obj_index = obj_index
+
+        if dist == np.inf:
+            color = self.background
+        else:
+            color = self.objects[closes_obj_index].color
+
+        return color
+
     @timed
-    def render(self, width, height):
-        pixel_array = np.zeros(shape=(width, height, 3), dtype=np.uint8)
-
-        for ray, j, k in self.camera.generate_rays(width, height):
-
-            dist = np.inf
-            closes_obj_index = 0
-            for obj_index, obj in enumerate(self.objects):
-                dist_ray = intersect_ray_sphere(ray, obj)
-
-                if dist_ray < dist:
-                    dist = dist_ray
-                    closes_obj_index = obj_index
-
-            if dist == np.inf:
-                color = np.array([0, 0, 0])
-            else:
-                color = self.objects[closes_obj_index].color
-
-            pixel_array[j, k] = color
-
-        return pixel_array
+    def render(self, RD):
+        result = np.apply_along_axis(self.trace, 0, RD)
+        return result
 
     def plot(self):
+        from mpl_toolkits.mplot3d import Axes3D
+        import matplotlib.pyplot as plt
+
         fig = plt.figure()
         ax = Axes3D(fig)
 
@@ -138,10 +138,13 @@ class Scene:
         plt.show()
 
 
-def intersect_ray_sphere(ray, sphere):
-    a = np.dot(ray.dir, ray.dir)
-    b = 2 * np.dot(ray.dir, ray.origin - sphere.origin)
-    c = np.dot(ray.origin - sphere.origin, ray.origin - sphere.origin) - sphere.radius * sphere.radius
+def intersect_ray_sphere(ray_origin, ray_dir, sphere_origin, sphere_radius):
+
+    RO_SO = ray_origin - sphere_origin
+
+    a = np.dot(ray_dir, ray_dir)
+    b = 2 * np.dot(ray_dir, RO_SO)
+    c = np.dot(RO_SO, RO_SO) - sphere_radius * sphere_radius
 
     discriminant = b * b - 4 * a * c
 
@@ -158,26 +161,37 @@ def intersect_ray_sphere(ray, sphere):
         return np.inf
 
 
-def main():
+def main(plotting=False):
     # Resolution settings:
-    w = 800
-    h = 600
+    w = 1000
+    h = 1000
 
     # Create scene with camera:
     scene = Scene()
 
-    # Add a sphere object in the middle:
-    sphere = Sphere(origin=np.array([10, 0, 0]), radius=1, color=np.array([200, 85, 85]))
+    # Add a sphere object:
+    sphere = Sphere(origin=np.array([8, 0, 0]), radius=1.1, color=np.array([200, 85, 85]))
     scene.add_object(sphere)
 
+    # Calculate the ray directions
+    print('Generating rays...')
+    RD = scene.camera.create_rays(w, h)
+
     # Render the image:
-    pixel_array = scene.render(w, h)
+    print('Rendering...')
+    result = scene.render(RD)
 
     # Save the image to a .png file:
+    pixel_array = np.zeros(shape=(result.shape[1], result.shape[2], 3), dtype=np.uint8)
+    for RGB in range(3):
+        pixel_array[:, :, RGB] = result[RGB, :, :]
     Image.fromarray(np.rot90(pixel_array)).save('output.png')
 
     # Plot scene setup:
-    scene.plot()
+    if plotting:
+        scene.plot()
+
+    return 0
 
 
 if __name__ == '__main__':
